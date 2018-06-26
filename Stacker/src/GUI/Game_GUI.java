ebf62c2e90f470b6e5144666c40042fef79d1f50
TheProgrammer2 <lukaswindisch@aon.at>
2018-06-26 10:45:39 +0200

    Added score display

--- Stacker/src/GUI/Game_GUI.java
+++ Stacker/src/GUI/Game_GUI.java
@@ -14,6 +14,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
+import java.util.ConcurrentModificationException;
 import java.util.LinkedList;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -28,7 +29,7 @@ public class Game_GUI extends javax.swing.JFrame {
     int[] movingBlocks = new int[10];
 
     int leftMoving = 0;
-    long movingSpeed = 100;
+    long movingSpeed = 80;
     boolean moveRight = true;
     int topMoving;
     boolean doesMove = true;
@@ -153,9 +154,14 @@ public class Game_GUI extends javax.swing.JFrame {
         g2d.drawImage(background, 0, 0, pnlScreen);
 
         //painting falling blocks
-        for (FallingBlock f : fallingBlocks) {
-            g2d.drawImage(blueBlock, f.leftX, f.topY, pnlScreen);
-            //g2d.fillRect(f.leftX, f.topY, blockWidth, blockWidth);
+        try {
+            for (FallingBlock f : fallingBlocks) {
+                g2d.drawImage(blueBlock, f.leftX, f.topY, pnlScreen);
+                //g2d.fillRect(f.leftX, f.topY, blockWidth, blockWidth);
+            }
+        } catch (ConcurrentModificationException e) {
+            System.out.println("Modified while printing!");
+            return;
         }
 
         // painting the fixed blocks
@@ -174,6 +180,11 @@ public class Game_GUI extends javax.swing.JFrame {
                 //g2d.fillRect(boundary + blockWidth * (leftMoving + i), topMoving, blockWidth, blockWidth);
             }
         }
+
+        //print score
+        g2d.setFont(new Font("Arial", Font.BOLD, 25));
+        g2d.setColor(Color.white);
+        g2d.drawString("Score: " + score, 20, 50);
 
         if (isGameOver) {
             g2d.setFont(new Font("Arial", Font.BOLD, 40));
